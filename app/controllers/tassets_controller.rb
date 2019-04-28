b1fa153e1b719fea00120f9b8d1052325b232d07
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-04-28 13:33:55 -1000

    Search assets with wildcard (SQL %)

--- app/controllers/tassets_controller.rb
+++ app/controllers/tassets_controller.rb
@@ -1,12 +1,14 @@
 class TassetsController < ApplicationController
     def index
+        # ToDo: search getting complicated.  Maybe move to model...
+        # especially if dealing with user syntax (?) to SQL syntax (%)
         @search_field = params[:search] ? params[:search] : "id"
         if params[:search]
-            #puts "Searching for #{params["search"]} #{params["value"]}"
+            puts "Searching for #{params["search"]} #{params["value"]}"
             if params["search"] == "status"
                 params["value"] = AssetStatus.get_code(params["value"])
             end
-            @assets = TAsset.where( params["search"] => params['value']).all
+            @assets = TAsset.where(query_string(params["search"]), params["value"])
         else
             @assets = TAsset.all 
         end
@@ -16,4 +18,12 @@ class TassetsController < ApplicationController
         @assets = TAsset.where( status: '3' ).all
     end
 
+    private
+    # why doing this weird thing?  Because the ? won't work for
+    # the column name (SQL treating the column and value name strings differently)
+    # A more secure approach would be to assign query string in case statement
+    # (so we are sure search_field is valid)
+    def query_string search_field
+        "#{search_field} LIKE ?"
+    end
 end
